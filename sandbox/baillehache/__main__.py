675b70a6df2b43ae414ec0501b6ff0c339053082
Baillehache Pascal <baillehache.pascal@gmail.com>
2025-01-20 10:13:09 +0900

    sandbox/baillehache: add bounding boxes of AOI calmayo, las penas, villa alpina

--- sandbox/baillehache/__main__.py
+++ sandbox/baillehache/__main__.py
@@ -14,8 +14,11 @@ print(f"image resolution: {preprocessor.resolution}m/px")
 # Areas of interest
 area_cordoba_city = LongLatBBox(-64.3, -64.2, -31.4, -31.3)
 area_los_medanitos = LongLatBBox(-65.7, -65.6, -31.6, -31.5)
-areas = [area_cordoba_city, area_los_medanitos]
-area_lbls = ["cordoba", "los_medanitos"]
+area_calmayo = LongLatBBox(-64.53518510984365, -64.36758169478006, -32.09793491720308, -31.98599080824592)
+area_las_penas = LongLatBBox(-64.19504913677835, -63.92899759153784, -30.67255559541076, -30.43974550819874)
+area_villa_alpina = LongLatBBox(-64.8501456212574, -64.70773302600543, -32.06252532553296, -31.93373503424498)
+areas = [area_cordoba_city, area_los_medanitos, area_calmayo, area_las_penas, area_villa_alpina]
+area_lbls = ["cordoba", "los_medanitos", "calmayo", "las_penas", "villa_alpina"]
 
 # Days of interests
 day_1 = "2023-12-01"

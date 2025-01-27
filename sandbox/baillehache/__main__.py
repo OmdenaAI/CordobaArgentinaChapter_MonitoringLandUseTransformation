f7769ad11ab2a803424c8d0a7c6deb97c01e95d4
Pascal Baillehache <baillehachepascal@gmail.com>
2025-01-27 16:25:06 +0900

    sandbox/baillehache, refactor test driving script

--- sandbox/baillehache/__main__.py
+++ sandbox/baillehache/__main__.py
@@ -9,14 +9,65 @@ from cordobaPredictor import *
 # Create a preprocessor instance
 preprocessor = CordobaDataPreprocessor()
 # Select the data source
-#preprocessor.select_source(CordobaDataSource.SENTINEL2)
-preprocessor.select_source(CordobaDataSource.LANDSAT8)
+preprocessor.select_source(CordobaDataSource.SENTINEL2)
+#preprocessor.select_source(CordobaDataSource.LANDSAT8)
+if preprocessor.data_source == CordobaDataSource.LANDSAT8:
+    preprocessor.step_search_image = 10
+    preprocessor.nb_median_composite = 1
 
 print(f"data source: {preprocessor.data_source}")
 print(f"image resolution: {preprocessor.resolution}m/px")
 
 # Create a predictor
 predictor = CordobaPredictor()
+
+def test_analyse_period(areas, area_lbls, days):
+
+    # Loop on pair of area of interest and day of interest
+    for i_area, area in enumerate(areas):
+        print(f"=== {area_lbls[i_area]}")
+
+        # Get the images
+        images = preprocessor.get_registered_images(days, area)
+
+        # For each image
+        for i_image in range(len(images)):
+            print(f"{images[i_image]}")
+
+            # Save the RGB bands to a png file
+            rgb = images[i_image].toRGB(gamma=0.66)
+            path_rgb = f"./Data/{preprocessor.data_source}_{area_lbls[i_area]}_{days[i_image]}_rgb.png"
+            print(f"save image to {path_rgb}")
+            Image.fromarray(rgb).save(path_rgb)
+
+            # Save the NDVI band to a png file
+            ndvi = images[i_image].toNDVI()
+            path_ndvi = f"./Data/{preprocessor.data_source}_{area_lbls[i_area]}_{days[i_image]}_ndvi.png"
+            print(f"save image to {path_ndvi}")
+            Image.fromarray(ndvi).save(path_ndvi)
+
+            # Save the EVI band to a png file
+            evi = images[i_image].toEVI()
+            path_evi = f"./Data/{preprocessor.data_source}_{area_lbls[i_area]}_{days[i_image]}_evi.png"
+            print(f"save image to {path_evi}")
+            Image.fromarray(evi).save(path_evi)
+
+            # Save the NDBI band to a png file
+            ndbi = images[i_image].toNDBI()
+            path_ndbi = f"./Data/{preprocessor.data_source}_{area_lbls[i_area]}_{days[i_image]}_ndbi.png"
+            print(f"save image to {path_ndbi}")
+            Image.fromarray(ndbi).save(path_ndbi)
+
+            # From the second image
+            if i_image > 0:
+                # If the NDVI index is high enough
+                if images[i_image].mean_ndvi > 0.2:
+                    # Predict the deforestation relative to the previous image
+                    deforest_image = predictor.getPcaKMeanClustering(
+                        [images[i_image-1], images[i_image]])
+                    path_deforest = f"./Data/{preprocessor.data_source}_{area_lbls[i_area]}_{days[i_image]}_deforest.png"
+                    print(f"save image to {path_deforest}")
+                    Image.fromarray(deforest_image).save(path_deforest)
 
 # Areas of interest
 area_cordoba_city = LongLatBBox(-64.3, -64.2, -31.4, -31.3)
@@ -29,52 +80,13 @@ all_area_lbls = ["cordoba", "los_medanitos", "calmayo", "las_penas", "villa_alpi
 areas = [area_calmayo, area_las_penas, area_villa_alpina]
 area_lbls = ["calmayo", "las_penas", "villa_alpina"]
 
+areas = [area_villa_alpina]
+area_lbls = ["villa_alpina"]
+
 # Days of interests
 # Recommended time windows:
 # Summer Time: 21 Dec - 21 March
 # Winter Time: 21 June - 21 Sept
-day_1 = "2019-08-01"
-day_2 = "2023-08-01"
-days = [day_1, day_2]
+days = ["2019-08-01", "2020-08-01", "2021-08-01"]
 
-# Loop on pair of area of interest and day of interest
-for i_area, area in enumerate(areas):
-    print(f"=== {area_lbls[i_area]}")
-
-    # Get the images
-    images = preprocessor.get_registered_images(days, area)
-    print(f"downloaded:\n{images[0]}\n{images[1]}\n")
-
-    # For each image
-    for i_image in range(2):
-
-        # Save the RGB bands to a png file
-        rgb = images[i_image].toRGB(gamma=0.66)
-        path_rgb = f"./Data/{preprocessor.data_source}_{area_lbls[i_area]}_{days[i_image]}_rgb.png"
-        print(f"save image to {path_rgb}")
-        Image.fromarray(rgb).save(path_rgb)
-
-        # Save the NDVI band to a png file
-        ndvi = images[i_image].toNDVI()
-        path_ndvi = f"./Data/{preprocessor.data_source}_{area_lbls[i_area]}_{days[i_image]}_ndvi.png"
-        print(f"save image to {path_ndvi}")
-        Image.fromarray(ndvi).save(path_ndvi)
-
-        # Save the EVI band to a png file
-        evi = images[i_image].toEVI()
-        path_evi = f"./Data/{preprocessor.data_source}_{area_lbls[i_area]}_{days[i_image]}_evi.png"
-        print(f"save image to {path_evi}")
-        Image.fromarray(evi).save(path_evi)
-
-        # Save the NDBI band to a png file
-        ndbi = images[i_image].toNDBI()
-        path_ndbi = f"./Data/{preprocessor.data_source}_{area_lbls[i_area]}_{days[i_image]}_ndbi.png"
-        print(f"save image to {path_ndbi}")
-        Image.fromarray(ndbi).save(path_ndbi)
-
-    # Predict the deforestation
-    deforest_image = predictor.getPcaKMeanClustering(images)
-    path_deforest = f"./Data/{preprocessor.data_source}_{area_lbls[i_area]}_{days[i_image]}_deforest.png"
-    print(f"save image to {path_deforest}")
-    Image.fromarray(deforest_image).save(path_deforest)
-   
+test_analyse_period(areas, area_lbls, days[:1])
